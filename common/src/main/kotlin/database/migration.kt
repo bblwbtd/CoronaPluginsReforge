dad31975564b0fec23eea6b188333922c31b43e1
BuildTools <unconfigured@null.spigotmc.org>
2022-02-24 21:07:46 +0800

    Update migration.kt

--- common/src/main/kotlin/database/migration.kt
+++ common/src/main/kotlin/database/migration.kt
@@ -3,8 +3,8 @@ package database
 import org.bukkit.plugin.Plugin
 import org.flywaydb.core.Flyway
 
-fun Plugin.migrateDB() {
+fun Plugin.migrateDB(vararg location: String) {
     val dataSource = getDataSource()
-    val flyway = Flyway.configure().dataSource(dataSource).load()
+    val flyway = Flyway.configure().dataSource(dataSource).locations(*location).load()
     flyway.migrate()
 }
