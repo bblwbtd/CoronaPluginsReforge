7f119ca3d499fd7c1ab66f65f9a18db35a625b3d
bblwbtd <gmyxgmyx@outlook.com>
2022-07-12 20:28:52 +0800

    Update dependence

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,25 +32,18 @@ allprojects {
         testImplementation(kotlin("test"))
         implementation(kotlin("stdlib-jdk8"))
         compileOnly("org.spigotmc:spigot-api:1.18.1-R0.1-SNAPSHOT")
-        testImplementation("com.github.seeseemelk:MockBukkit-v1.17:1.7.0")
+        testImplementation("com.github.seeseemelk:MockBukkit-v1.17:1.13.0")
         testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
-        implementation("com.github.ajalt.clikt:clikt:3.2.0")
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.+")
+        implementation("com.github.ajalt.clikt:clikt:3.5.0")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
     }
 
     tasks.test {
         useJUnitPlatform()
     }
 
-    tasks.withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "16"
-    }
     val compileKotlin: KotlinCompile by tasks
     compileKotlin.kotlinOptions {
-        jvmTarget = "16"
-    }
-    val compileTestKotlin: KotlinCompile by tasks
-    compileTestKotlin.kotlinOptions {
         jvmTarget = "16"
     }
 }
