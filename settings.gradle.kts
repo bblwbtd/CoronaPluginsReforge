993f6cdfa24b0686fc75ecdff0fbe3259baed8ec
bblwbtd <gmyxgmyx@outlook.com>
2023-07-31 16:44:25 -0400

    Update gradle settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,8 @@
 rootProject.name = "CoronaPluginsReforge"
 
 
-include(":auth")
-include(":common")
-include(":tpmachine")
-include(":friends")
-include(":depositbox")
+include("auth")
+include("common")
+include("tpmachine")
+include("friends")
+include("depositbox")
